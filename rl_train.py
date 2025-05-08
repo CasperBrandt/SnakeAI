12fe69dd08b1aa119126a62d424d7dae5ac97d11
CasperBrandt <Caspermbrandt@gmail.com>
2025-05-08 15:54:07 +0200

    Using EvalCallback to evaluate and save the best trained model

--- rl_train.py
+++ rl_train.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
 import os
 from snakeEnv import SnakeEnv
 
@@ -15,12 +16,12 @@ if not os.path.exists(logdir):
     
 env = SnakeEnv()
 
+eval_env = SnakeEnv()
+eval_callback = EvalCallback(eval_env, best_model_save_path=models_dir, eval_freq=10000, deterministic=True, render=False)
+
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir, device='cpu')
 #model = PPO.load(model_path, env=env, device='cpu') # Swap to this line if you wanna start training a pre-existing modell
 
-TIMESTEPS = 25000
-i = 1
+TIMESTEPS = 50000
 while True:
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{TIMESTEPS*i}")
-    i += 1
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, callback=eval_callback, tb_log_name="PPO")
